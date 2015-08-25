0e1a43e189e14aa3871afb93a90beec8dd7c7b40
Pei-Tang Huang <tangtheone@gmail.com>
2015-08-25 14:41:06 +0800

    Update 2015-08-25T06:41:01.480Z

---
+++ js/docs-setup.js
@@ -0,0 +1,53 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "c.focus.directive:cFocusOn",
+      "shortName": "cFocusOn",
+      "type": "directive",
+      "moduleName": "c.focus",
+      "shortDescription": "當cFocusOn 的變數名稱為true時，則focus進入該欄位。",
+      "keywords": "$scope $timeout angular api app c-focus-on cfocuson clickme controller data-ng-click directive focus focusme function input input1 inputval module ng-controller ng-model test text true type var"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/node_modules/jquery/dist/jquery.min.js",
+      "js/node_modules/marked/lib/marked.js",
+      "js/node_modules/angular/angular.min.js",
+      "js/node_modules/angular-animate/angular-animate.min.js",
+      "js/src/focus.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": false,
+      "angularAnimate": false,
+      "marked": false,
+      "prettify": false
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/node_modules/jquery/dist/jquery.min.js",
+    "js/node_modules/marked/lib/marked.js",
+    "js/node_modules/angular/angular.min.js",
+    "js/node_modules/angular-animate/angular-animate.min.js",
+    "js/src/focus.js"
+  ]
+};
